4e5c9e8e406211c708ba2f1a997362a578f3af44
ZSL-1024 <53318298+ZSL-1024@users.noreply.github.com>
2021-05-24 14:35:40 +0800

    Add files via upload

---
+++ chapter_03_Lists/bicycle.py
@@ -0,0 +1,19 @@
+# List
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+print(bicycles[1])
+
+print(bicycles[3])
+
+print(bicycles[-1])
+
+message = "My fisst bicycle was a " + bicycles[0].title() + "."
+
+print(message)
---
+++ chapter_03_Lists/cars.py
@@ -0,0 +1,33 @@
+# Sorting a List Permanently with the sort() Method
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+# Sorting a List Temporarily with the sorted() Function
+
+print("\nHere is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+# Printing a List in Reverse Order
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+# Finding the Length of a List
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
